13395671c65458bf21db51f3a9ac52f0ddf7a5f7
guilhermeperitto <perittoguilherme@gmail.com>
2024-09-04 12:58:04 -0300

    criando bases com classes genericas para automatizar cruds padroes

--- src/main/java/com/mintgestao/MintGestao.java
+++ src/main/java/com/mintgestao/MintGestao.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MintGestao {
     public static void main(String[] args) {
         SpringApplication.run(MintGestao.class, args);
-        //Pong.pong();
+        Pong.pong();
     }
 }
