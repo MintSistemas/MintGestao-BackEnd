cf9b516342e1d7388eb79b3f0c2ce178e48ca57d
GuilhermePeritto <perittoguilherme@gmail.com>
2024-09-04 23:45:09 -0300

    a

--- src/main/java/com/mintgestao/MintGestao.java
+++ src/main/java/com/mintgestao/MintGestao.java
@@ -1,6 +1,5 @@
 package com.mintgestao;
 
-import com.mintgestao.Infrastructure.util.Pong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MintGestao {
     public static void main(String[] args) {
         SpringApplication.run(MintGestao.class, args);
-        Pong.pong();
     }
 }
