77fc8431108931a242383c2458c0181f7cca3f12
guilhermeperitto <perittoguilherme@gmail.com>
2024-09-23 11:19:50 -0300

    validação horaini sempre maior que horafim no cadastro de evento

--- src/main/java/com/mintgestao/Domain/Entity/Evento.java
+++ src/main/java/com/mintgestao/Domain/Entity/Evento.java
@@ -6,11 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.TenantId;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.UUID;
+import jakarta.validation.constraints.AssertTrue;
 
 @Entity
 @NoArgsConstructor
@@ -20,28 +20,39 @@ public class Evento {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     @NotBlank(message = "Nome é obrigatório e deve ter entre 3 e 100 caracteres")
     @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     private String nome;
+
     @NotBlank(message = "Sobrenome é obrigatório e deve ter entre 3 e 100 caracteres")
     @Size(min = 3, max = 100, message = "Sobrenome deve ter entre 3 e 100 caracteres")
     private String sobrenome;
+
     @NotBlank(message = "Email é obrigatório")
     @Email(message = "Insira um email válido")
     private String email;
+
     @NotBlank(message = "Telefone é obrigatório")
     private String telefone;
+
     @NotNull(message = "Valor total é obrigatório")
     @Positive(message = "Valor total deve ser maior que zero")
     private double valortotal;
+
     @NotNull(message = "Valor hora é obrigatório")
     @Positive(message = "Valor hora deve ser maior que zero")
     private double valorhora;
-    @NotNull(message = "Hora inicial é obrigatório")
+
+    @NotNull(message = "Hora inicial é obrigatória")
+    @FutureOrPresent
     private LocalTime horainicio;
-    @NotNull(message = "Hora final é obrigatório")
+
+    @NotNull(message = "Hora final é obrigatória")
+    @Future
     private LocalTime horafim;
-    @NotNull(message = "Data é obrigatório")
+
+    @NotNull(message = "Data é obrigatória")
     @Future(message = "Não é possível cadastrar um evento com a data menor que a atual")
     private LocalDate dataevento;
 
@@ -57,4 +68,13 @@ public class Evento {
 
     @TenantId
     private Integer idtenant;
+
+    // Método de validação customizado
+    @AssertTrue(message = "A hora final deve ser maior que a hora inicial")
+    public boolean isHoraFimMaiorQueHoraInicio() {
+        if (horainicio != null && horafim != null) {
+            return horafim.isAfter(horainicio);
+        }
+        return true;
+    }
 }
